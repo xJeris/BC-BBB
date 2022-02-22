d8d2cd482ca15875ca3b5ddfe76960d36b7028b8
xJeris <85350673+xJeris@users.noreply.github.com>
2022-02-22 10:05:44 -0500

    test

--- static/js/charts.js
+++ static/js/charts.js
@@ -123,7 +123,7 @@ function buildCharts(sample) {
           size: [200, 400, 600, 800, 1000, 1200],
           sizemode: 'area'
           },
-        text: 'OTU: ' + selSample.otu_ids
+        text: selSample.otu_labels
       }
     ];
 
@@ -137,6 +137,7 @@ function buildCharts(sample) {
       }
     };
 
+    console.log("update: 10:06");
     // 3. Use Plotly to plot the data with the layout.
     Plotly.newPlot('bubble', bubbleData, bubbleLayout);
 
